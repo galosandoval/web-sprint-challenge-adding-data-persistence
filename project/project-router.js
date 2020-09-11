a396accaa80156ee8b1e8d9bac3bcb653bfb5be9
Galo Sandoval <galosan92@gmail.com>
2020-09-11 11:06:20 -0700

    stuck on last post

--- project/project-router.js
+++ project/project-router.js
@@ -39,4 +39,40 @@ router.get("/task", (req, res) => {
     });
 });
 
+// add resource
+router.post('/resource', (req, res) => {
+  const resource = req.body
+  db('resource').insert(resource, 'id')
+  .then((resources) => {
+    res.status(200).json(resources);
+  })
+  .catch((error) => {
+    res.status(500).json({ error: error.message });
+  });
+})
+
+// add project
+router.post('/', (req, res) => {
+  const newProject = req.body
+  db('project').insert(newProject, 'id')
+  .then((project) => {
+    res.status(200).json(project);
+  })
+  .catch((error) => {
+    res.status(500).json({ error: error.message });
+  });
+})
+
+// add task
+router.post('/task', (req, res) => {
+  const newTask= res.body
+  db('task').insert(newTask, 'id')
+  .then((task) => {
+    res.status(200).json(task);
+  })
+  .catch((error) => {
+    res.status(500).json({ error: error.message });
+  });
+})
+
 module.exports = router;
